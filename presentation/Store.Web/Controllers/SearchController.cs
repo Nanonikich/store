7375ddda53f67af0f202019e4f91f82d37b7c2a5
Your Name <you@example.com>
2021-03-17 18:00:48 +0300

    Добавить страницу поиска

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IInstrumentRepository instrumentRepository;
+
+        public SearchController(IInstrumentRepository instrumentRepository)
+        {
+            this.instrumentRepository = instrumentRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var instruments = instrumentRepository.GetAllByTitle(query);
+            return View(instruments);
+        }
+    }
+}
