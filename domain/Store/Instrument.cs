da0d8c945a7c6542cfa2cc00531f3b94510799f8
Your Name <you@example.com>
2021-03-17 16:26:42 +0300

    Описать сущность Instrument

---
+++ domain/Store/Instrument.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Store
+{
+    public class Instrument
+    {
+        public int Id { get; }
+
+        public string Title { get; }
+
+        public Instrument(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+
+    }
+}
