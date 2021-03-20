c15c7586483ecd4e08b6d744ed86f78db3eb91ae
Your Name <you@example.com>
2021-03-20 14:55:30 +0300

    Добавить инструмент в корзину

---
+++ presentation/Store.Web/Controllers/CartController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IInstrumentRepository instrumentRepository;
+
+        public CartController(IInstrumentRepository instrumentRepository)
+        {
+            this.instrumentRepository = instrumentRepository;
+        }
+        public IActionResult Add(int id)
+        {
+            var instrument = instrumentRepository.GetById(id);
+            Cart cart;
+            if (!HttpContext.Session.TryGetCart(out cart))
+                cart = new Cart();
+            if (cart.Items.ContainsKey(id))
+                cart.Items[id]++;
+            else
+                cart.Items[id] = 1;
+
+            cart.Amount += instrument.Price;
+
+            HttpContext.Session.Set(cart);
+
+            return RedirectToAction("Index", "Instrument", new { id });
+        }
+    }
+}
